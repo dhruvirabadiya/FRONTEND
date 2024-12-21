0de725ded6294494510b0592a2cced6a68c44977
avinash <arakholiya5@gmail.com>
2024-12-21 17:55:23 +0530

    layout

--- javascript/operators.js
+++ javascript/operators.js
@@ -1,12 +1,75 @@
 // Arethmetic operator
-{
-    let x = 30;
-    let y = 2;
+// {
+//     let x = 30;
+//     let y = 2;
 
-    console.log(x + y);
-    console.log(x - y);
-    console.log(x * y);
-    console.log(x / y);
-    console.log(x % y);
-    console.log(x ** y);
-}
+//     console.log(x + y);
+//     console.log(x - y);
+//     console.log(x * y);
+//     console.log(x / y);
+//     console.log(x % y);
+//     console.log(x ** y);
+// }
+
+
+// Assignment operator
+// {
+   
+//     let x = 5
+//     let y = 3
+//     console.log(x = y);
+//     console.log(x -= y);
+//     console.log(x += y);
+//     console.log(x *= y);
+//     console.log(x /= y);
+//     console.log(x **= y);
+//     console.log(x %= y); 
+// }
+
+// Comparision operator
+// {
+//     let x = 1000
+//     let y = 1000
+
+//     console.log(x == y);
+//     console.log(x != y);
+//     console.log(x === y);
+//     console.log(x !== y);
+//     console.log(x < y);
+//     console.log(x > y);
+//     console.log(x <=  y);
+//     console.log(x >= y);    
+// }
+
+// Logical operator
+// {
+//     let x = true
+//     let y = false
+//     let z = true
+
+//     console.log(x && y);
+//     console.log(x && y && z);
+//     console.log(x || y || z);
+//     console.log(!(x && y) && !(z || x) || !(x && z));  
+    
+// }
+
+//Ternery operator
+// statement ? condition1 : condition2
+// {
+//     let age = 18
+//     age >= 18 ? console.log('you are aligable') : console.log('you are not aligable');
+
+// }
+
+// typeOf operator
+// console.log(typeof 12);
+
+// Bitwise operator
+// {
+//     let x = 14
+//     let y = 17
+
+//     console.log(x & y);
+    
+// }
