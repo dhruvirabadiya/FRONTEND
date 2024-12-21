c48f6b83786c0c1b03abe6547fd1310481376e98
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2024-12-21 16:43:39 +0530

    layout

---
+++ javascript/operators.js
@@ -0,0 +1,12 @@
+// Arethmetic operator
+{
+    let x = 30;
+    let y = 2;
+
+    console.log(x + y);
+    console.log(x - y);
+    console.log(x * y);
+    console.log(x / y);
+    console.log(x % y);
+    console.log(x ** y);
+}
