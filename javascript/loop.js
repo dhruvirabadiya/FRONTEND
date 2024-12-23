d4ccc4d8ccfdeba2f240717f13bacb344a4a5a77
avinash <arakholiya5@gmail.com>
2024-12-23 18:52:06 +0530

    loop

---
+++ javascript/loop.js
@@ -0,0 +1,23 @@
+/* Javascript Loops */
+
+/*
+
+Loops are used to execute the same block of code again and again, as long as a certain condition is true.
+
+In JavaScript we have the following types of loops:
+
+1. for loop
+2. while loop
+3. do...while loop
+4. for...in loop
+5. for...of loop
+
+*/
+
+// 1. for loop
+
+// syntax :
+
+// for(initialization ; condition ; increment/decrement){
+//     // code to be execute
+// }
--- javascript/statement.js
+++ javascript/statement.js
@@ -1,35 +1,35 @@
 // javascript statement
 
-// if......statement 
-// if.....else.....statement 
-// if.....else.....if statement 
-// nested if ...else statement 
+// if......statement
+// if.....else.....statement
+// if.....else.....if statement
+// nested if ...else statement
 // switch case statment
 
-// if......statement 
+// if......statement
 // {
 //     let storage = true
 
 //     if(storage == true){
-//         console.log('production has been started!!!!');    
+//         console.log('production has been started!!!!');
 //     }
- // return console.log('Storage is empty!!'); // Illegal return statement
+// return console.log('Storage is empty!!'); // Illegal return statement
 // }
 
-// if.....else.....statement 
+// if.....else.....statement
 // {
 //     let age = 18
 
 //     if(age>=18){
 //         console.log('eligable for vote');
-        
+
 //     }else{
-//         console.log('not eligable for what');      
+//         console.log('not eligable for what');
 //     }
 // }
 
 // {
-//     let age = 18 
+//     let age = 18
 //     let card = true
 
 //     if(age>=18 && card == true){
@@ -39,11 +39,10 @@
 //     }
 // }
 
-
-// nested if ...else statement 
+// nested if ...else statement
 // {
 //     let age = 18
-//     let card = true 
+//     let card = true
 
 //     if(age >= 18){
 //         if(card==true){
@@ -56,16 +55,53 @@
 //     }
 // }
 
-// if.....else.....if statement 
+// if.....else.....if statement
 
-{
-    let book ="matsh"
+// {
+//     let book ="chemistry"
 
-    if(book == 'chemistry'){
-        console.log('chemistry book is available');       
-    }else if(book == 'biology'){
-        console.log('physics book is available');
-    }else if(book == 'english'){
-        console.log('english book is available');
-    }
-}
+//     if(book === 'chemistry'){
+//         console.log('chemistry book is available');
+//     }else if(book === 'biology'){
+//         console.log('biology book is available');
+//     }else if(book === 'english'){
+//         console.log('english book is available');
+//     }else if(book === 'physics'){
+//         console.log('physics book is available');
+//     }else if(book === 'drawing'){
+//         console.log('physics book is available');
+//     }else{
+//         console.log('book is not available');
+//     }
+// }
+
+// switch case statment
+
+// {
+//   let book = "gujrati";
+
+//   switch (book) {
+//     case "chemistry":
+//       console.log("chemistry book");
+//       break;
+
+//     case "english":
+//       console.log("english book");
+//       break;
+
+//     case "biology":
+//       console.log("biology book");
+//       break;
+
+//     case "matsh":
+//       console.log("matsh book");
+//       break;
+
+//     case "sanskrit":
+//       console.log("sanskrit book");
+//       break;
+
+//     default:
+//       console.log("book is not available");
+//   }
+// }
