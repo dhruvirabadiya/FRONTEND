afcdbae11cddd8da01246b40c8f85c81d966e15d
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2024-12-24 16:29:21 +0530

    tasks

---
+++ javascript/tasks/statements&loops/loops program.js
@@ -0,0 +1,3 @@
+// -->program-1
+// Write a program that calculates the sum of all numbers from 1 to 100.
+
---
+++ javascript/tasks/statements&loops/statement program.js
@@ -0,0 +1,69 @@
+//--> program-1
+// Check if a number is positive, negative, or zero
+// let num = 5
+
+// if (num > 0) {
+//     console.log("number is positive");
+// } else if (num < 0) {
+//     console.log("number is negative");
+// } else {
+//     console.log("number is zero");
+// }
+
+
+//--> program-2
+// Check if a person is eligible to vote
+// let age = 18
+
+// if (age >= 18) {
+//     console.log("you are eligible for vote");
+// } else {
+//     console.log("you are not eligable for vote");
+// }
+
+//--> program-3
+// Determine the largest of three numbers
+// let num1 = 10;
+// let num2 = 20;
+// let num3 = 30;
+
+// let largest;
+// if (num1 >= num2 && num1 >= num3) {
+//     largest = num1;
+// } else if (num2 >= num1 && num2 >= num3) {
+//     largest = num2;
+// } else {
+//     largest = num3;
+// }
+
+// console.log("The largest number is:", largest);
+
+//--> program-4
+// Check if a year is a leap year
+
+// let year = 2024;
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     console.log(year + " is a leap year.");
+// } else {
+//     console.log(year + " is not a leap year.");
+// }
+
+//--> program-5
+// Determine the grade based on a score
+// let score = 50
+// let grade
+
+// if (score >= 90) {
+//     grade = "A"
+// } else if (score >= 80) {
+//     grade = "B"
+// } else if (score >= 70) {
+//     grade = "C"
+// } else if (score >= 60) {
+//     grade = "D"
+// } else {
+//     grade = "F"
+// }
+
+// console.log("Score:", score, "Grade:", grade);
