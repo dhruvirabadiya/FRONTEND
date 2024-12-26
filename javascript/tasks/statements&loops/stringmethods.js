9d5cf31c1df7751faad681d1adcc8463fddf1151
avinash <arakholiya5@gmail.com>
2024-12-26 17:59:38 +0530

    stringmethods

---
+++ javascript/tasks/statements&loops/stringmethods.js
@@ -0,0 +1,49 @@
+// Javascript String Method 
+// String.prototype.at()
+// at(index)
+
+{
+    let str = "this is javascript string method!"
+               012345678901234567890123456789012
+               123456789012345678901234567890123
+
+               console.log(str.at(-12));
+               console.log(str.at(12));
+               
+}
+
+// String.prototype.CharAt()
+
+// charAt(index)
+{
+    let str = "this is javascript string method!"
+               012345678901234567890123456789012
+               123456789012345678901234567890123
+
+               console.log(str.charAt(10));
+               
+}
+
+// String.prototype.charCodeAt()
+
+// charCodeAt(index)
+{
+    let str = "This is Javascript String Method!"
+               012345678901234567890123456789012
+               123456789012345678901234567890123
+
+        console.log(str.charCodeAt(10));
+}
+
+// String.prototype.concat()
+
+// concat(str1)
+// concat(str1, str2)
+// concat(str1, str2, /* …, */ strN)
+{
+    let x = "Hello"
+    let y = "javascript"
+
+    console.log(x.concat(y));
+    
+}
