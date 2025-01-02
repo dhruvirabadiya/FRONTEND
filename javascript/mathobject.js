a5868f88403f30c2052785e98ab22e32a904f44f
avinash <arakholiya5@gmail.com>
2025-01-02 18:54:07 +0530

    math and date obj method

---
+++ javascript/mathobject.js
@@ -0,0 +1,64 @@
+//math object methods
+
+//Math.abs
+let math = Math.abs(10)
+console.log(math);
+
+//Math.ceil
+let math1 = Math.ceil(10.50)
+console.log(math1);
+
+//Math.cos
+let math2 = Math.cos(90*3.14/180)
+console.log(math2);
+
+//Math.sign
+let math11 = Math.sin(90*3.14/180)
+console.log(math11);
+
+
+//Math.floor
+let math3 = Math.floor(10.50)
+console.log(math3);
+
+//Math.max
+array = [10,20,30,10,40,50]
+// let math4 = Math.max(...array)
+let math4 = Math.max(10,20,30,40,10,50)
+console.log(math4);
+
+//Math.min
+array1 = [10,10,20,30,40,50]
+// let math5 = Math.min(...array1)
+let math5 = Math.min(10,10,20,30,40,50)
+console.log(math5);
+
+//Math.log
+let math6 = Math.log(2)
+console.log(math6);
+
+// Math.log10
+let math7 = Math.log10(2)
+console.log(math7);
+
+//Math.log2
+let math8 = Math.log2(2)
+console.log(math8);
+
+//Math.pow
+let math9 = Math.pow(5,2)
+console.log(math9);
+
+//Math.random
+let math10 = Math.random()*10
+console.log(math10);
+
+//Math.trunc
+let math12 = Math.trunc(50.25)
+console.log(math12);
+
+//Math.pi
+let math13 = Math.PI
+console.log(math13);
+
+
