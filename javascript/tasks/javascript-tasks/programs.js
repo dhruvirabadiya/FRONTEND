0a3d4a87055f3db5665a96170d5e32db2602fe97
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2025-01-02 16:48:54 +0530

    js tasks

--- javascript/tasks/javascript-tasks/programs.js
+++ javascript/tasks/javascript-tasks/programs.js
@@ -90,21 +90,39 @@
 // }
 
 // 13)JavaScript Program to Check Prime Number
-let num = 11
-if (num <= 1) {
-    console.log(`${num} is not a prime number.`);
-} else {
-    let isPrime = true;
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            isPrime = false
-            break
-        }
-    } if (isPrime) {
-        console.log(`${num} is a prime number.`);
-    } else {
-        console.log(`${num} is not a prime number.`);
-    }
-}
+// let num = 11
+// if (num <= 1) {
+//     console.log(`${num} is not a prime number.`);
+// } else {
+//     let isPrime = true;
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             isPrime = false
+//             break
+//         }
+//     } if (isPrime) {
+//         console.log(`${num} is a prime number.`);
+//     } else {
+//         console.log(`${num} is not a prime number.`);
+//     }
+// }
 
+// 14) JavaScript Program to Print All Prime Numbers in an Interval
+// let start = 5
+// let end = 20
 
+// for (let num = start; num <= end; num++) {
+//     if (num < 2) {
+//         console.log(`${num} is not a prime number.`);
+//     } else {
+//         let isPrime = true
+//         for (let i = 2; i < num; i++) {
+//             if (num % i === 0) {
+//                 isPrime = false
+//             }
+//         }
+//         if (isPrime) {
+//             console.log(`${num}`);
+//         }
+//     }
+// }
