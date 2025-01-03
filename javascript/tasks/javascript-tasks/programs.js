acacfd0a0c79a35f1d9ae78899abafa140e74b1e
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2025-01-03 16:46:22 +0530

    js tasks

--- javascript/tasks/javascript-tasks/programs.js
+++ javascript/tasks/javascript-tasks/programs.js
@@ -126,3 +126,46 @@
 //         }
 //     }
 // }
+
+// 15)JavaScript Program to Find the Factorial of a Number
+// let num = 5
+// let factorial = 1
+
+// for (let i = 1; i <= num; i++) {
+//     factorial *= i
+// }
+// console.log(`The factorial of ${num} is ${factorial}.`);
+
+// 16)JavaScript Program to Display the Multiplication Table
+// let num = 5
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+// 17)JavaScript Program to Print the Fibonacci Sequence
+// let num1 = 0
+// let num2 = 1
+// let next
+// for (let i = 1; i <= 10; i++) {
+//     console.log(num1);
+//     next = num1 + num2
+//     num1 = num2
+//     num2 = next
+// }
+
+// 18)JavaScript Program to Check Armstrong Number
+// let num = 153
+// let sum = 0
+// let temp = num
+
+// while (temp > 0) {
+//     let digit = temp % 10
+//     sum += digit * digit * digit
+//     temp = Math.floor(temp / 10)
+// }
+
+// if (sum === num) {
+//     console.log(`${num} is an Armstrong number.`);
+// } else {
+//     console.log(`${num} is not an Armstrong number.`);
+// }
