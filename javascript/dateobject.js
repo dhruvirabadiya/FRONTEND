6b9f3832adee745e76be0ca396fc62ac8a14e41b
avinash <arakholiya5@gmail.com>
2025-01-03 18:22:32 +0530

    dateobjectmethods

--- javascript/dateobject.js
+++ javascript/dateobject.js
@@ -1,2 +1,15 @@
-let today = new Date()
+// Javascript Date Object Method
+// let today = new Date()
+// console.log(today);
+
+// let today = new Date('1 jan 1970')
+// console.log(today);
+
+let today = Date.parse('01 jan 2025 21:00:00')
 console.log(today);
+
+let demo = today.getDate()
+console.log(demo);
+
+
+    
