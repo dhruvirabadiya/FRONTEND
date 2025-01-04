652b652ff28461db274a3f723c4d63269b831789
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2025-01-04 16:48:17 +0530

    javascript

--- javascript/dateobject.js
+++ javascript/dateobject.js
@@ -5,11 +5,46 @@
 // let today = new Date('1 jan 1970')
 // console.log(today);
 
-let today = Date.parse('01 jan 2025 21:00:00')
-console.log(today);
+// let today = Date.parse('01 jan 2025 21:00:00')
+// console.log(today);
 
-let demo = today.getDate()
-console.log(demo);
+//get methods
+// let today1 = new Date()
+// console.log(today1);
 
+// let demo = today1.getDate()
+// console.log(demo);
 
-    
+// let demo1 = today1.getMonth()
+// console.log(demo1);
+
+// let demo2 = today1.getFullYear()
+// console.log(demo2);
+
+// let demo3 = today1.getDay()
+// console.log(demo3);
+
+// let demo4 = today1.getHours()
+// console.log(demo4);
+
+// let demo5 = today1.getMinutes()
+// console.log(demo5);
+
+// let demo6 = today1.getSeconds()
+// console.log(demo6);
+
+// let demo7 = today1.getMilliseconds()
+// console.log(demo7);
+
+//set methods
+// let today = new Date()
+
+// today.setDate(11)
+// today.setFullYear(2026)
+// today.setMonth(1)
+// today.setHours(12)
+// today.setMinutes(30)
+// today.setSeconds(59)
+// today.setMilliseconds(1000)
+
+// console.log(today);
---
+++ javascript/numbermethod.js
@@ -0,0 +1,19 @@
+/* Javascript Number Method */
+// Number values represent floating-point numbers like 37 or -9.25.
+// The Number constructor contains constants and methods for working with numbers. Values of other types can be converted to numbers using the Number() function.
+
+// new Number(value)
+// Number(value)
+
+{
+    let x = "10"
+    let y = Number(x)
+    console.log(y);
+
+    console.log(typeof x);
+    console.log(typeof y);
+}
+
+{
+
+}
