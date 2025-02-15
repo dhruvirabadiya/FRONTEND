c973d5b122c2527e6925f1ae1fbacf25526aaf10
avinash <arakholiya5@gmail.com>
2025-02-15 18:11:16 +0530

    traversing

--- j-query/traversing.js
+++ j-query/traversing.js
@@ -8,6 +8,26 @@ parentsUntil()
 
 */
 
+// $('.btn').click(function(){
+//     $('span').parent().css('background-color','red')
+// })
+
+// $('.btn').click(function(){
+//     $('span').parents().css('background-color','yellow')
+// })
+
+// $('.btn').click(function(){
+//     $('span').parentsUntil('.parentDiv').css('background-color','red')
+// })
+
 $('.btn').click(function(){
-    $('span').parent().css('background-color','red')
+    $('.parentUl').children().css('background-color','red')
+})
+
+$('.btn').click(function(){
+    $('.parentUl').find('.span').css('background-color' , 'red')
+})
+
+$('.btn').click(function(){
+    
 })
