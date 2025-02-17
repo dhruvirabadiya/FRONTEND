71525f7f238219daf26bf69dabe8f01fd6a08177
avinash <arakholiya5@gmail.com>
2025-02-17 18:08:14 +0530

    jquery

---
+++ j-query/jqueryhtml.js
@@ -0,0 +1,22 @@
+// jQuery HTML
+
+// $('#btn').click(function(){
+//     alert($('#heading').text())
+//     console.log($('#heading').text());
+// })
+
+// $('#btn').click(function(){
+//     alert($('#heading').html())
+//     console.log('#heading').html();
+    
+// })
+
+// $('#btn').click(function(){
+//         alert($('#text').val())
+//         console.log($('#text').val());
+// })
+
+$('#btn').click(function(){
+    $('#heading').html('<span>This is Head Tag</span>')
+    console.log($('#heading').html('<span>This is Head Tag</span>'));
+})
--- j-query/traversing.js
+++ j-query/traversing.js
@@ -20,14 +20,54 @@ parentsUntil()
 //     $('span').parentsUntil('.parentDiv').css('background-color','red')
 // })
 
-$('.btn').click(function(){
-    $('.parentUl').children().css('background-color','red')
-})
+// $('.btn').click(function(){
+//     $('.parentUl').children().css('background-color','red')
+// })
 
-$('.btn').click(function(){
-    $('.parentUl').find('.span').css('background-color' , 'red')
-})
+// $('.btn').click(function(){
+//     $('.parentUl').find('.span').css('background-color' , 'red')
+// })
 
-$('.btn').click(function(){
-    
-})
+// $('.btn').click(function(){
+//     $('.child-1').siblings().css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('span').siblings().css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('.parentUl').next().css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('.child-1').nextAll().css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('.child-1').nextUntill('li:nth-child(4)').css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('li:nth-child(4)').prev().css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('li:nth-child(4)').prevAll().css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('li:nth-child(4)').prevUntil('li:nth-child(2)').css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('span').first().css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('span').last().css('background-color' , 'red')
+// })
+
+// $('.btn').click(function(){
+//     $('span').eq(0).css('background-color' , 'red')
+// })
