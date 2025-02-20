bed7599601ca714750ec50cb397cc15c8501947b
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2025-02-20 16:42:42 +0530

    full-stack

---
+++ React/vite/my-app/src/problem-solve.jsx
@@ -0,0 +1,76 @@
+// Problem1
+// function Profile() {
+//     return (
+//       <img
+//         src="https://i.imgur.com/lICfvbD.jpg"
+//         alt="Aklilu Lemma"
+//       />
+//     );
+//   }
+
+// function Profile() {
+//     return (
+//         <img
+//             src="https://i.imgur.com/lICfvbD.jpg"
+//             alt="Aklilu Lemma"
+//         />
+//     );
+// }
+
+// export default Profile
+
+//problem-2
+// export default function Profile() {
+//     return (
+//         <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />
+//     )
+// }
+
+//Problem-3
+// function profile() {
+//     return (
+//       <img
+//         src="https://i.imgur.com/QIrZWGIs.jpg"
+//         alt="Alan L. Hart"
+//       />
+//     );
+//   }
+
+//   export default function Gallery() {
+//     return (
+//       <section>
+//         <h1>Amazing scientists</h1>
+//         <profile />
+//         <profile />
+//         <profile />
+//       </section>
+//     );
+//   }
+
+// function Profile() {
+//     return (
+//         <img
+//             src="https://i.imgur.com/QIrZWGIs.jpg"
+//             alt="Alan L. Hart"
+//         />
+//     );
+// }
+
+// export default function Gallery() {
+//     return (
+//         <section>
+//             <h1>Amazing scientists</h1>
+//             <Profile />
+//             <Profile />
+//             <Profile />
+//         </section>
+//     );
+// }
+
+//Problem-4
+// Write your component below!
+// export default function Myfunction() {
+//     return (
+//         <h1>Hello World</h1>
+//     )
+// }
