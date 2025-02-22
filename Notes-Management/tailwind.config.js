a3085ae57eff347d5bd97b96ec418ef7647b4b95
dhruvirabadiya <rabdiyadhruvi64@gmail.com>
2025-02-22 16:43:30 +0530

    notes-app

--- Notes-Management/tailwind.config.js
+++ Notes-Management/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        screens: {
+          sm: "768px",
+          md: "896px",
+          lg: "960px",
+          xl: "1065px",
+          "2xl": "1200px",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
